b08ae02e2bf49778c4d1e129b87a13af7a6a0274
Christopher-Bogan <60634997+Christopher-Xavier@users.noreply.github.com>
2020-10-06 15:45:55 -0400

    Update people.router.js

--- src/modules/people/people.router.js
+++ src/modules/people/people.router.js
@@ -1,25 +1,50 @@
-const express = require('express');
-const json = require('body-parser').json();
+const express = require('express')
+const json = express.json()
 
-const People = require('./people.service');
+const People = require('./people.service')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (req, res) => {
-  // Return all the people currently in the queue.
-  return res.send(People.get());
+router.get('/', async (req, res) => {
+	try {
+		// Return all the people currently in the queue.
+		const people = await People.get()
+
+		if (!people) {
+			return res.status(400).send({ error: 'Empty List' })
+		}
+		res.json(people)
+	} catch (error) {
+		console.log(error)
+	}
 })
 
-router.post('/', json, (req, res) => {
-  // Add a new person to the queue.
-  const newPerson = { ...req.body };
-  People.enqueue(newPerson.name);
-  res.status(204).end();
-});
+router.post('/', json, async (req, res) => {
+	try {
+		const { person } = req.body
+		if (!person) {
+			return res.status(400).send({ error: 'Invalid' })
+		}
+		const data = await People.enqueue(person)
 
-router.delete('/', (req, res) => {
-  People.dequeue();
-  res.status(204).end();
-});
+		res.status(201).json(data.data)
+	} catch (error) {
+		console.log(error)
+	}
+	// Add a new person to the queue.
+})
 
-module.exports = router;
+router.delete('/', async (req, res) => {
+	try {
+		const deleted = await People.dequeue()
+		const next = await People.enqueue('Next Person')
+		if (!deleted) {
+			return res.status(400).send({ error: `Empty List` })
+		}
+		res.status(204).end()
+	} catch (error) {
+		console.log(error)
+	}
+})
+
+module.exports = router
