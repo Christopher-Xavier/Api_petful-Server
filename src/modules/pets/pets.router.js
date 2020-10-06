509dfb18db0917234faae6ad21cf7875d57e33b6
Christopher-Bogan <60634997+Christopher-Xavier@users.noreply.github.com>
2020-10-06 15:48:52 -0400

    Update pets.router.js

--- src/modules/pets/pets.router.js
+++ src/modules/pets/pets.router.js
@@ -6,17 +6,50 @@ const People = require('../people/people.service')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  // Return all pets currently up for adoption.
-  const pets = Pets.get();
-  return res.send(pets)
+router.get('/dog', async (req, res) => {
+	// Return all pets currently up for adoption.
+	try {
+		const nextDog = await Pets.get()['Next Dog']
+		if (!nextDog) {
+			return res.status(400).send({ error: 'No dogs found' })
+		}
+		res.json(nextDog)
+	} catch (error) {
+		console.log(error)
+	}
 })
 
-router.delete('/:pet_type', json, (req, res) => {
-  // Remove a pet from adoption.
-  const type = req.params.pet_type;
-  Pets.dequeue(type)
-  return res.status(204).end()
+router.delete('/dog', json, async (req, res) => {
+	// Remove a pet from adoption.
+	try {
+		await Pets.dequeue('dogs')
+
+		res.sendStatus(204)
+	} catch (error) {
+		console.log(error)
+	}
+})
+
+router.get('/cat', async (req, res) => {
+	try {
+		const nextCat = await Pets.get()['Next Cat']
+		if (!nextCat) {
+			return res.status(400).send({ error: 'No cats found' })
+		}
+		res.json(nextCat)
+	} catch (error) {
+		console.log(error)
+	}
+})
+router.delete('/cat', json, async (req, res) => {
+	//
+	try {
+		await Pets.dequeue('cats')
+
+		res.sendStatus(204)
+	} catch (error) {
+		console.log(error)
+	}
 })
 
 module.exports = router
